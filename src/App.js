a2d829d95a32b46aadee298cb17dc95a245acff2
Aditya Singh <adityakrsingh1999@gmail.com>
2021-06-27 17:36:58 +0530

    Loading screen

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Footer from "./components/Footer";
 import ProjectHeader from "./components/ProjectHeader";
 import AboutMain from "./components/AboutMain";
 import Navbar from "./components/Navbar";
+import { useState, useEffect } from "react";
+import PacmanLoader from "react-spinners/PacmanLoader";
+import { Loading } from "./styles/components/LoadingStyle";
 
 const routes = [
   { path: "/", name: "Home", Component: Home },
@@ -16,25 +19,48 @@ const routes = [
 ];
 
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isPageLoaded, setIsPageLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (isLoaded) {
+      setIsPageLoaded(true);
+    }
+  }, [isLoaded]);
+
   return (
     <div className="App">
-      <Navbar />
-      {routes.map(({ path, Component }) => (
-        <Route key={path} exact path={path}>
-          {path === "/projects" ? (
-            <>
-              <ProjectHeader />
-              <Project />
-              <Footer />
-            </>
-          ) : (
-            <>
-              <Component />
-              <Footer />
-            </>
-          )}
-        </Route>
-      ))}
+      {!isPageLoaded ? (
+        <Loading>
+          <PacmanLoader color={"#36D7B7"} loading={isPageLoaded} size={25} />
+          <br />
+          LOADING
+        </Loading>
+      ) : (
+        <>
+          <Navbar />
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {path === "/projects" ? (
+                <>
+                  <ProjectHeader />
+                  <Project />
+                  <Footer />
+                </>
+              ) : (
+                <>
+                  <Component />
+                  <Footer />
+                </>
+              )}
+            </Route>
+          ))}
+        </>
+      )}
     </div>
   );
 }
---
+++ src/styles/components/LoadingStyle.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Loading = styled.div`
+  background-color: #0e0e0e;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  /* text-align: center; */
+  color: #fff;
+  font-family: Poppins, sans-serif;
+  font-size: 40px;
+  line-height: 32px;
+  font-weight: 800;
+  flex-direction: column;
+`;
