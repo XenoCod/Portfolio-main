edc41e82fd26295011151a71229fd67f56fe4922
Aditya Singh <adityakrsingh1999@gmail.com>
2021-07-05 15:14:44 +0530

    View mod

--- src/styles/components/Banner.js
+++ src/styles/components/Banner.js
@@ -1,27 +1,29 @@
 import styled from "styled-components";
 
 export const BannerContainer = styled.div`
-  height: 50vh;
-  margin-bottom: 5%;
+  /* height: 50vh; */
+  margin-bottom: 15%;
   display: flex;
   align-items: center;
   justify-content: center;
   padding-top: 5%;
   z-index: 1000;
+  /* margin-bottom: 90px; */
   @media only screen and (max-width: 700px) {
-    margin-bottom: 0;
-    padding-bottom: 0;
-    width: 70vw;
-    height: 70vh;
+    width: 100vw;
+    margin-right: 0;
+    transform: translateX(-15%);
+
+    /* height: 80vh; */
   }
 `;
 export const BannerSection = styled.div`
-  width: 50vw;
+  width: 70vw;
 
-  height: 50vh;
+  /* height: 50vh; */
   @media only screen and (max-width: 700px) {
     width: 100vw;
-    height: 70vh;
+    /* height: -90vh; */
     margin-bottom: 0;
     padding-bottom: 0;
   }
